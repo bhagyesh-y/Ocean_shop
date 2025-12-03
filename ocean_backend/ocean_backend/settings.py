981affef7db83acb5919470ab5df604eee63df96
bhagyesh508 <bhagyeshyadav29@gmail.com>
2025-12-03 18:25:37 +0530

    updated middleware

--- ocean_backend/ocean_backend/settings.py
+++ ocean_backend/ocean_backend/settings.py
@@ -63,8 +63,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware"
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -72,7 +73,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware"
+  
 ]
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  
